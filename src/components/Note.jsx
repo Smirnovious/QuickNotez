a0168623e96544fb582a4b247c4d3ae49d1ef6a1
Smirnovious <alex0401@gmail.com>
2022-08-24 19:04:18 +0300

    created note and notelist components

---
+++ src/components/Note.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {MdDeleteForever} from 'react-icons/md'
+const Note = () => {
+  return (
+    <>
+    <div className="Note">
+        <span>Hello this is our first note!</span>
+        <div className="note-footer">
+        <small>Created on: <span>12/12/2019</span></small>
+        <MdDeleteForever className='delete' size='1.3em'/>
+        </div>
+    </div>
+    </>
+    )
+}
+
+export default Note
---
+++ src/components/NotesList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Note} from './Note'
+const NotesList = () => {
+  return (
+    <div className="notes-list">
+      <Note/>
+    </div>
+  )
+}
+
+export default NotesList
