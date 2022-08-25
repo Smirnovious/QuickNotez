b75e1e915c1c27cd0fc51ca6dca8fed47abf3f66
Smirnovious <alex0401@gmail.com>
2022-08-25 09:06:58 +0300

    Changed note to dynamic data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NotesList from './components/NotesList'
-
+import { nanoid } from 'nanoid'
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+    id: nanoid(),
+    text: 'This is a new note',
+    date: "2020-11-21"
+  },
+  {
+    id: nanoid(),
+    text: 'This is a second note',
+    date: "2020-01-01"
+  },
+  {
+    id: nanoid(),
+    text: 'This is a third note',
+    date: "2020-02-16"
+  },
+])
   return (
     <div className='container'>
-      <NotesList/>
+      <NotesList notes={notes}/>
     </div>
   )
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import {MdDeleteForever} from 'react-icons/md'
 
 
-const Note = () => {
+const Note = ({id, text, date}) => {
   return (
     <>
     <div className="note">
-        <span>Hello this is our first note!</span>
+        <span>{text}</span>
         <div className="note-footer">
-        <small>Created on: <span>12/12/2019</span></small>
+        <small>Created on: <span>{date}</span></small>
         <MdDeleteForever className='delete' size='1.3em'/>
         </div>
     </div>
